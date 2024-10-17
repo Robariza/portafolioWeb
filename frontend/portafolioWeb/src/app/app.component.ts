23d1f10e9ed1e3afeb331a0f2f19fc34d2148291
Robariza <robinsonariza2001@gmail.com>
2024-10-17 00:38:45 -0500

    [update] components CSS | HTML  app, nav, banner, projects.

--- frontend/portafolioWeb/src/app/app.component.ts
+++ frontend/portafolioWeb/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from "./components/banner/banner.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { CommentsComponent } from "./components/comments/comments.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { AboutComponent } from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerComponent, NavComponent, ProjectsComponent, CommentsComponent, FooterComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
