7b7cbf6fff0d686521349e08f9432e7a92d4b23b
Robariza <robinsonariza2001@gmail.com>
2024-10-23 00:57:21 -0500

    [create] button contact, cv

---
+++ frontend/portafolioWeb/src/app/components/contact-button/contact-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-button.component.html',
+  styleUrl: './contact-button.component.css'
+})
+export class ContactButtonComponent {
+
+}
---
+++ frontend/portafolioWeb/src/app/components/cv-button/cv-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './cv-button.component.html',
+  styleUrl: './cv-button.component.css'
+})
+export class CvButtonComponent {
+
+}
