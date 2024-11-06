75700fe1ea35914615d8e2f1dcd416864af41f9b
Robariza <robinsonariza2001@gmail.com>
2024-11-05 20:53:31 -0500

    [update] about component

--- frontend/portafolioWeb/src/app/components/about/about.component.ts
+++ frontend/portafolioWeb/src/app/components/about/about.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { CarrouselAboutComponent } from "../carrousel-about/carrousel-about.component";
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CarrouselAboutComponent],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  currentView : string = 'view1';
 
+  changeView(view: string){
+    this.currentView = view;
+  }
 }
