4dd6ab4e795d961221993cfb3777ffaea4cf9846
Robariza <robinsonariza2001@gmail.com>
2024-11-07 18:33:11 -0500

    [update] about, banner components form

--- frontend/portafolioWeb/src/app/components/banner/banner.component.ts
+++ frontend/portafolioWeb/src/app/components/banner/banner.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { ContactButtonComponent } from "../contact-button/contact-button.component";
 import { CvButtonComponent } from "../cv-button/cv-button.component";
+import { FormContactComponent } from "../form-contact/form-contact.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [ContactButtonComponent, CvButtonComponent],
+  imports: [ContactButtonComponent, CvButtonComponent, FormContactComponent, CommonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+  isModalOpen = false;
 
+  showModal() {
+    this.isModalOpen = true;
+  }
+
+  hideModal() {
+    this.isModalOpen = false;
+  }
 }
