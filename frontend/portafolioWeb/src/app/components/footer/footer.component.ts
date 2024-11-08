b65f5d9765ca418201b552ae194b51169cc5437e
Robariza <robinsonariza2001@gmail.com>
2024-11-08 15:35:45 -0500

    [update] footer component form

--- frontend/portafolioWeb/src/app/components/footer/footer.component.ts
+++ frontend/portafolioWeb/src/app/components/footer/footer.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { ContactButtonComponent } from "../contact-button/contact-button.component";
+import { FormContactComponent } from "../form-contact/form-contact.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactButtonComponent],
+  imports: [ContactButtonComponent, CommonModule, FormContactComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  isModalOpen = false;
 
+  showModal() {
+    this.isModalOpen = true;
+  }
+
+  hideModal() {
+    this.isModalOpen = false;
+  }
 }
