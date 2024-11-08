f9c24c97743ea398d2975437523793853315edcb
Robariza <robinsonariza2001@gmail.com>
2024-11-08 15:51:30 -0500

    [update] nav component form

--- frontend/portafolioWeb/src/app/components/nav/nav.component.ts
+++ frontend/portafolioWeb/src/app/components/nav/nav.component.ts
@@ -1,18 +1,28 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormContactComponent } from "../form-contact/form-contact.component";
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormContactComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   isScrolled = false;
   isMenuOpen = false;
+  isModalOpen = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.pageYOffset > 50;
+  }
+
+  showModal() {
+    this.isModalOpen = true;
+  }
+
+  hideModal() {
+    this.isModalOpen = false;
   }
 }
