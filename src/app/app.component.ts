5cba6ad124486ea59b5533b0a622de725434148e
Robariza <robinsonariza2001@gmail.com>
2024-11-22 19:56:13 -0500

    [update] path local project nav app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from "./components/banner/banner.component";
 import { NavComponent } from "./components/nav/nav.component";
@@ -6,7 +6,7 @@ import { ProjectsComponent } from "./components/projects/projects.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { AboutComponent } from "./components/about/about.component";
 import { CommentComponent } from "./components/comment/comment.component";
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +15,23 @@ import { CommentComponent } from "./components/comment/comment.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // Escuchar los cambios en el fragmento
+    this.route.fragment.subscribe(fragment => {
+      // Si el fragmento es válido, realizar el scroll a la sección
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          // Scroll suave hacia el elemento con el id
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
+
   title = 'portafolioWeb';
 }
